50f5b084e2786642ea09d6b838b818d93b11bb05
mathensousaa <mhenrique.sousa43@gmail.com>
2023-02-03 01:56:57 -0300

    Fixing error Command npm run vercel-build exide with 1

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,16 @@ import App from './_app.js';
 import Image from 'next/image';
 
 
+
 function Home( ) {
     return (
         <>
             <Head>
                 <title>Industrial Service</title>
-
+                
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link 
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
-                    rel="stylesheet"
-                />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
             </Head>
 
             <Header/>
