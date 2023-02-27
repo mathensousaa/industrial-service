7fea8b3454b12128b7cf5b5b23dc32156034da0f
Matheus de Sousa <mhenrique.sousa43@gmail.com>
2023-02-27 10:13:49 -0300

    old code

--- pages/components/Servicos/Services.js
+++ pages/components/Servicos/Services.js
@@ -1,7 +1,45 @@
+import styles from './Services.module.css';
+import Icon from '../Icon/Icon';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+
+
 export default function Services() {
     return (
-        <section>
-            <h1>Serviços</h1>
+        <section className={styles.services}>
+            <div className={styles.servicesContainer}>
+                <div className={styles.servicesCol}>
+                    <h2>Nossos Serviços</h2>
+                    <p>Ofereçemos soluções completas e personalizadas em segurança e manutenção industrial e predial</p>
+                    <div className={styles.servicesList}>
+                        <div className={styles.servicesItem}>
+                            <Icon
+                                size="40px"
+                                bgColor="var(--white)"
+                                icon={faCircleCheck}
+                                iconSize="2x"
+                                iconColor="var(--red-400)"
+
+                            />
+                            <h3>Acesso por cordas</h3>
+                        </div>
+                        <div className={styles.servicesItem}>
+                            <h3>Resgate em altura e espaço confinado</h3>
+                        </div>
+                        <div className={styles.servicesItem}>
+                            <h3>Treinamentos</h3>
+                        </div>
+                        <div className={styles.servicesItem}>
+                            <h3>Manutenção predial</h3>
+                        </div>
+                        <div className={styles.servicesItem}>
+                            <h3>Manutenção industrial</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.servicesImage}>
+
+                </div>
+            </div>
         </section>
     )
 }
