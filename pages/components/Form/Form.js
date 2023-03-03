9c760f733415b5b3d119de446905990db83749d7
mathensousaa <mhenrique.sousa43@gmail.com>
2023-03-03 11:34:13 -0300

    feat: adding budget and work with us forms

--- pages/components/Form/Form.js
+++ pages/components/Form/Form.js
@@ -1,13 +1,32 @@
+import styles from './Form.module.scss'
+import React from 'react';
+
 export default function Form () {
+    const [showBudget, setShoBudget] = React.useState(false);
+    const [showWork, setShowWork] = React.useState(true);
+
     return (
         <div>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <fieldset className={styles.formSwitch}>
+                <input type='radio' name='switcher' id='budget' checked="checked"></input>
+                <label for='budget'>Orçamento</label>
+                <input type='radio' name='switcher' id='workWithUS'></input>
+                <label for='workWithUS'>Trabalhe conosco</label>
+            </fieldset>
+            <div>
+                {
+                    showBudget && 
+                    <form className={styles.budgetForm}>
+                        Orçamento
+                    </form>
+                }
+                {
+                    showWork && 
+                    <form className={styles.workForm}>
+                        Trabalhe conosco
+                    </form>
+                }
+            </div>
         </div>
     )
 }
